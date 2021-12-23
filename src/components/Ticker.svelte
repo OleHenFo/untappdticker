36551e686401a94821b27f146804d47b48bdf03b
OleHenFo <olehenrik3@gmail.com>
2021-12-23 01:27:35 +0100

    animation ok+

--- src/components/Ticker.svelte
+++ src/components/Ticker.svelte
@@ -1,7 +1,8 @@
 <script>
   import {auth_token,checkins} from "../store";
   import { flip } from 'svelte/animate';
-	import { slide,fade,scale } from 'svelte/transition';
+	import { slide,fade,fly } from 'svelte/transition';
+  import { backInOut } from 'svelte/easing';
 
   let shown = [];
 
@@ -26,8 +27,8 @@
 <p>Auth token is {$auth_token}</p>
 
 <div class='container'>
-{#each $checkins.slice(-5) as ch (ch.nr)}
-  <div in:fade out:fade animate:flip class='checkin' style="order: -{ch.nr};">
+{#each $checkins.slice(-4) as ch (ch.nr)}
+  <div in:fly={{y:-120,duration:1600,easing:backInOut}} out:fly={{y:120,duration:1400,easing:backInOut}} animate:flip={{duration:1500,easing:backInOut}} class='checkin' style="order: {ch.nr};">
     <p>Name: {ch.name}, nr {ch.nr}</p>
   </div>
 {/each}
@@ -37,12 +38,19 @@
   .container {
     display: flex;
     flex-direction: column-reverse;
-    background-color:khaki;
-    height: 20vmax;
-    overflow-y: hidden;
+    align-items: center;
+
+    height: auto;
   }
 
   .checkin {
     background-color: cyan;
+    height: 120px;
+    width: 80vw;
+    min-width: 650px;
+
+    border-radius: 2rem;
+    margin: 2rem;
+    padding: 1rem 2rem;
   }
 </style>
