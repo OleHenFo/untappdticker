217056e8ca9b71c86e7164964c4e3c0e485adaa9
OleHenFo <olehenrik3@gmail.com>
2021-12-26 22:00:11 +0100

    styling

--- src/components/Ticker.svelte
+++ src/components/Ticker.svelte
@@ -37,18 +37,35 @@
     });
   };
 
-  getCheckins()
+  // getCheckins()
   // setInterval(() => {
   //   getCheckins()
-  // }, 60000);
+  // }, 60000); TODO: uncomment
 </script>
 
 <p>Auth token is {$auth_token}</p>
 
+<!-- <script>
+      let userName = ch.user.user_name;
+      let avatar = ch.user.user_avatar;
+      let beerName = ch.beer.beer_name;
+      let beerLabel = ch.beer.beer_label;
+      let beerStyle = ch.beer.beer_style;
+      let breweryName = ch.brewery.brewery_name;
+      let comment = ch.checkin_comment;
+      let time = ch.created_at;
+      let score = ch.rating_score;
+      let media = ch.media.items;
+    </script> -->
+
 <div class='container'>
-{#each $checkins.slice(-4) as ch (ch.nr)}
+{#each $checkins.slice(-4) as ch (ch.checkin_id)}
   <div in:fly={{y:-120,duration:1600,easing:backInOut}} out:fly={{y:120,duration:1400,easing:backInOut}} animate:flip={{duration:1500,easing:backInOut}} class='checkin' style="order: {ch.nr};">
-    <p>Name: {ch.name}, nr {ch.nr}</p>
+    <img class="avatar" src={ch.user.user_avatar} alt={ch.user.user_name}>
+    <div class="text-container">
+      <span class="username">{ch.user.user_name}</span>
+      <p class="comment">{ch.checkin_comment}</p>  
+    </div>
   </div>
 {/each}
 </div>
@@ -70,6 +87,30 @@
 
     border-radius: 2rem;
     margin: 2rem;
-    padding: 1rem 2rem;
+    padding: 1rem 1rem;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .avatar {
+    border-radius: 100%;
+    margin-right: 1.5rem;;
+    width: 100px;
+    height: 100px;
+  }
+
+  .text-container {
+    flex-grow: 4;
+  }
+
+  .username {
+    color: red;
+    font-size: 2rem;
+  }
+
+  .comment {
+    color: red;
   }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,24 @@
 import { writable } from 'svelte/store';
 
-export let auth_token = writable(0);
+let storedToken = localStorage.getItem("auth_token");
+if (storedToken==null||storedToken==0||storedToken==""){
+  storedToken = 0;
+}
+
+export let auth_token = writable(storedToken);
+auth_token.subscribe(value => {
+  localStorage.setItem("auth_token", value );
+});
+
+
 export let code = writable(0);
 export let venue = writable(0);
-export let checkins = writable([]);
+
+let storedCheckins = JSON.parse(localStorage.getItem("checkins"));
+if (storedCheckins==null||storedCheckins==0||storedCheckins==""||storedCheckins==[]||storedCheckins=={}){
+  storedCheckins = [];
+}
+export let checkins = writable(storedCheckins);
+checkins.subscribe(value => {
+  localStorage.setItem("checkins", JSON.stringify(value) );
+});
